1261c9015ba334849525c759735c79cc14fd71e0
Dawid Pasek <dpasek@prolan.com.pl>
2020-07-22 12:34:40 +0200

    [Feature]: Changes in project architecture

--- transaction-service-api/src/main/java/com/example/transactionservice/service/TransactionRequest.java
+++ transaction-service-api/src/main/java/com/example/transactionservice/service/TransactionRequest.java
@@ -1,7 +1,12 @@
 package com.example.transactionservice.service;
+// [TODO]: Your package should be unique for every project but your group id should be same in every your project
+// [TODO]: Package name should be <group.id>.<project.name>.<module.name>.<directory.name>. It helps with trace error stack, recognise your projects/modules etc
+// [TODO]: Click PPM on package and select refactor > Rename... (shift+F6)
+
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+// [TODO]: Remove unused imports, also you can press alt+enter and select option 'Enable optimize imports on the fly to make Intellij do it automatically'
 import org.springframework.stereotype.Service;
 
 @Service
--- transaction-service/src/test/java/com/example/transactionservice/TransactionServiceApplicationTests.java
+++ transaction-service/src/test/java/com/example/transactionservice/TransactionServiceApplicationTests.java
@@ -1,4 +1,8 @@
 package com.example.transactionservice;
+// [TODO]: Your package should be unique for every project but your group id should be same in every your project
+// [TODO]: Package name should be <group.id>.<project.name>.<module.name>.<directory.name>. It helps with trace error stack, recognise your projects/modules etc
+// [TODO]: For test packages it should have .test ending
+// [TODO]: Click PPM on package and select refactor > Rename... (shift+F6)
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
