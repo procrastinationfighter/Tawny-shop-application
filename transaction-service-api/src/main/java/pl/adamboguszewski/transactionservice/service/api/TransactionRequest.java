9b5afb9b94744315fbe04ef112659de957581c99
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-07-27 11:49:59 +0200

    Removed redundant @Service in TransactionRequest.

--- transaction-service-api/src/main/java/pl/adamboguszewski/transactionservice/service/api/TransactionRequest.java
+++ transaction-service-api/src/main/java/pl/adamboguszewski/transactionservice/service/api/TransactionRequest.java
@@ -1,9 +1,5 @@
 package pl.adamboguszewski.transactionservice.service.api;
 
-import org.springframework.stereotype.Service;
-
-// [TODO]: @Service is not necessary here, we don't need to inject this class anywhere. It's just a normal class
-@Service
 public class TransactionRequest {
 
     private final String id;
