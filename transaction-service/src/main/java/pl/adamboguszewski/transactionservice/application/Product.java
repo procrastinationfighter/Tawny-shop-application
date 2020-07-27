5279c66206bc2da0f5e572c477803bfb6696ca82
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-07-27 12:58:29 +0200

    Product class basics.

--- transaction-service/src/main/java/pl/adamboguszewski/transactionservice/application/Product.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transactionservice/application/Product.java
@@ -2,4 +2,28 @@ package pl.adamboguszewski.transactionservice.application;
 
 // [TODO]: Same as in Transaction class, what info does products in the shop have
 public class Product {
+
+    private final String id;
+
+    private final String name;
+
+    private final double price;
+
+    public Product(String id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
