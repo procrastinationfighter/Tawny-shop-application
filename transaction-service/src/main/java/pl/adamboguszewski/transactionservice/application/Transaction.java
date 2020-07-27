8ee9cddd338267518aabbb0458a7f4b2779d5ca1
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-07-27 13:13:15 +0200

    Basics of Transaction class.

--- transaction-service/src/main/java/pl/adamboguszewski/transactionservice/application/Transaction.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transactionservice/application/Transaction.java
@@ -1,9 +1,31 @@
 package pl.adamboguszewski.transactionservice.application;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // [TODO]: Imagine you make a transaction in the shop or somewhere. Create transaction model class which will represent every information needed (for example list of products)
 public class Transaction {
 
-    List<Product> products;
+    private final List<Product> products;
+
+    private double totalPrice;
+
+    private final String id;
+
+    public Transaction(List<Product> products, String id) {
+        this.id = id;
+        this.products = new ArrayList<>();
+        for(Product product : products) {
+            this.products.add(product);
+            totalPrice += product.getPrice();
+        }
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
