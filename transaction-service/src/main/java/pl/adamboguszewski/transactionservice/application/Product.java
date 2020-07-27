98f3ab7e4bd41b36d01034deab3dfef4e5da004e
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-07-27 16:05:50 +0200

    Remove name property from Product.

--- transaction-service/src/main/java/pl/adamboguszewski/transactionservice/application/Product.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transactionservice/application/Product.java
@@ -4,26 +4,15 @@ public class Product {
 
     private final String id;
 
-    private String name;
-
     private double price;
 
-    public Product(String id, String name, double price) {
+    public Product(String id, double price) {
         this.id = id;
-        this.name = name;
         this.price = price;
     }
 
     public String getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public double getPrice() {
