be6e5049c47ab5c33907d17adba0535078b469c4
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-07-28 12:01:12 +0200

    Implement TransactionPayment.

--- transaction-service/src/main/java/pl/adamboguszewski/transactionservice/application/TransactionPayment.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transactionservice/application/TransactionPayment.java
@@ -1,5 +1,40 @@
 package pl.adamboguszewski.transactionservice.application;
 
 public class TransactionPayment {
-    // [TODO]: Implement this class.
+
+    public enum Currency {
+        PLN,
+        USD,
+        EUR
+    }
+
+    public enum PaymentType {
+        CASH,
+        CARD,
+        GOOGLE_PAY
+    }
+
+    private final Long amount;
+
+    private final Currency currency;
+
+    private final PaymentType paymentType;
+
+    public TransactionPayment(Long amount, Currency currency, PaymentType paymentType) {
+        this.amount = amount;
+        this.currency = currency;
+        this.paymentType = paymentType;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public PaymentType getPaymentType() {
+        return paymentType;
+    }
 }
