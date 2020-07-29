f271cafce7194528a428d14883c0f5382d147713
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-07-29 12:15:07 +0200

    Remove checkoutId from Transaction (it is already in TransactionInfo).

--- transaction-service/src/main/java/pl/adamboguszewski/transactionservice/application/Transaction.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transactionservice/application/Transaction.java
@@ -17,19 +17,16 @@ public class Transaction {
 
     UUID transactionId;
 
-    UUID checkoutId;
-
     TransactionInfo transactionInfo;
 
     public Transaction(List<TransactionProduct> products, Long id, UUID transactionId,
-                       Long totalPrice, UUID checkoutId, TransactionInfo transactionInfo) {
+                       Long totalPrice, TransactionInfo transactionInfo) {
         // [TODO]: If product has a list with objects like this transaction, this will cause problems.
         this.id = id;
         this.products = new ArrayList<>();
         this.products.addAll(products);
         this.transactionId = transactionId;
         this.totalPrice = totalPrice;
-        this.checkoutId = checkoutId;
         this.transactionInfo = transactionInfo;
     }
 
