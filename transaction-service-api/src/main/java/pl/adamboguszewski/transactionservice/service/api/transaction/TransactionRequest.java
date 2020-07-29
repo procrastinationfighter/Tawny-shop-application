7be9de8d1507466081ec4ddcefb8d8a3a58f0a16
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-07-29 12:22:17 +0200

    Rebuild TransactionRequest.

--- transaction-service-api/src/main/java/pl/adamboguszewski/transactionservice/service/api/transaction/TransactionRequest.java
+++ transaction-service-api/src/main/java/pl/adamboguszewski/transactionservice/service/api/transaction/TransactionRequest.java
@@ -1,14 +1,30 @@
 package pl.adamboguszewski.transactionservice.service.api.transaction;
 
 import lombok.Value;
+import lombok.NonNull;
+
+import java.util.List;
+import java.util.UUID;
 
 // [TODO]: This is a create transaction request, implement info needed to create a transaction
 // [TODO]: Also look for a more proper class name, the hint is above
 @Value
 public class TransactionRequest {
 
-    String id;
+    @NonNull
+    Long totalPrice;
 
-    String name;
+    @NonNull
+    List<Product> products;
+
+    @Value
+    public static class Product {
+
+        @NonNull
+        UUID productId;
+
+        @NonNull
+        Long quantity;
+    }
 
 }
