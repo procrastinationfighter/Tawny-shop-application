ddc006d38e21d6290d8159109fc2c1b4a005f9e0
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-07-29 12:48:37 +0200

    Remove TODOs.

--- transaction-service-api/src/main/java/pl/adamboguszewski/transactionservice/service/api/transaction/CreateTransactionResponse.java
+++ transaction-service-api/src/main/java/pl/adamboguszewski/transactionservice/service/api/transaction/CreateTransactionResponse.java
@@ -5,8 +5,6 @@ import lombok.Value;
 
 import java.util.UUID;
 
-// [TODO]: This is application response, implement info that should be returned to application user after you create and add transaction to database (we don't have db yet but nvm)
-// [TODO]: Also look for a more proper class name to fit the request
 @Value
 public class CreateTransactionResponse {
 
