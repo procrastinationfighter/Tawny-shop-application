2c28e1fcd2dd2c3eabe020d151b6fd76c68a79c2
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-08-01 23:42:34 +0200

    Create TransactionService with empty methods.

---
+++ transaction-service/src/main/java/pl/adamboguszewski/transaction/service/application/TransactionService.java
@@ -0,0 +1,30 @@
+package pl.adamboguszewski.transaction.service.application;
+
+import pl.adamboguszewski.transaction.service.api.transaction.CreateTransactionRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// [TODO] Fill the details of this class.
+public class TransactionService {
+
+    public List<Transaction> getAll() {
+        return new ArrayList<>();
+    }
+
+    public Transaction getById(Long id) {
+        return null;
+    }
+
+    public Transaction createTransaction(CreateTransactionRequest request) {
+        return null;
+    }
+
+    public String editTransaction(Long id) {
+        return "";
+    }
+
+    public void deleteTransaction(Long id) {
+
+    }
+}
