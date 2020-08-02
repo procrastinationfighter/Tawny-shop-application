b174a51601c99db1ba009cf3c88985abc444a6cc
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-08-02 19:31:00 +0200

    Create TransactionDto.fromRequest method.

--- transaction-service/src/main/java/pl/adamboguszewski/transaction/service/application/dto/TransactionDto.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transaction/service/application/dto/TransactionDto.java
@@ -1,11 +1,14 @@
 package pl.adamboguszewski.transaction.service.application.dto;
 
 import lombok.Value;
+import pl.adamboguszewski.transaction.service.api.transaction.CreateTransactionRequest;
+import pl.adamboguszewski.transaction.service.application.Transaction;
 import pl.adamboguszewski.transaction.service.application.TransactionInformation;
 import pl.adamboguszewski.transaction.service.application.TransactionProduct;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Value
 public class TransactionDto {
@@ -26,5 +29,21 @@ public class TransactionDto {
         this.totalPrice = totalPrice;
         this.transactionInformation = transactionInformation;
         this.products = products;
+    }
+
+    public static TransactionDto fromRequest(CreateTransactionRequest request) {
+        TransactionInformation transactionInformation = TransactionInformation.fromRequest(request.getTransactionInformation());
+
+        List<TransactionProduct> products = request.getProducts()
+                .stream()
+                .map(TransactionProduct::fromRequest)
+                .collect(Collectors.toList());
+
+        return new TransactionDto(
+                request.getTransactionId(),
+                request.getTotalPrice(),
+                transactionInformation,
+                products
+        );
     }
 }
