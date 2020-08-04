694e5e8d4a4203a95794fae82d174a450aa1a782
Dawid Pasek <dpasek@prolan.com.pl>
2020-08-04 10:57:02 +0200

    Add feature todo

--- transaction-service/src/main/java/pl/adamboguszewski/transaction/service/application/TransactionService.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transaction/service/application/TransactionService.java
@@ -12,6 +12,20 @@ import java.util.Optional;
 @Service
 public class TransactionService {
 
+    // [TODO]
+/*
+    First things first -- we need database to work with so install postgres https://www.postgresql.org/ and read
+    how to do this. Try to create some tables in client (I know that pgAdmin exists, you can try with other), get used
+    to postgres. Idk if you had contact with MySql but it is very similar database.
+    Next thing is to access data with our application. We will use JPA and Hibernate. Read about this and about ORM,
+    then you can follow this guide to extend JpaRepository https://spring.io/guides/gs/accessing-data-jpa/
+    Next job is to inject our Repository to this service and call suitable methods to get/insert data to connected
+    datasource. It is one of the hardest step to understand in developing this application. Other references:
+    https://docs.spring.io/spring-data/data-commons/docs/1.6.1.RELEASE/reference/html/repositories.html
+    https://docs.spring.io/spring-data/jpa/docs/1.5.0.RELEASE/reference/html/jpa.repositories.html
+    https://www.baeldung.com/spring-data-repositories
+*/
+
     public List<Transaction> getAll() {
         return Collections.emptyList();
     }
