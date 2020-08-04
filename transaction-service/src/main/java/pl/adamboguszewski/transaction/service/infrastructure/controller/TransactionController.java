d2c6979bc1242e3c081f73e1cf8a63cc5bae7c03
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-08-04 17:00:05 +0200

    Add validation while creating a new transaction.

--- transaction-service/src/main/java/pl/adamboguszewski/transaction/service/infrastructure/controller/TransactionController.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transaction/service/infrastructure/controller/TransactionController.java
@@ -11,6 +11,7 @@ import pl.adamboguszewski.transaction.service.application.Transaction;
 import pl.adamboguszewski.transaction.service.application.TransactionService;
 import pl.adamboguszewski.transaction.service.application.dto.TransactionDto;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
@@ -34,7 +35,7 @@ public class TransactionController {
     }
 
     @PostMapping("")
-    public CreateTransactionResponse createTransaction(CreateTransactionRequest request) {
+    public CreateTransactionResponse createTransaction(@Valid @RequestBody CreateTransactionRequest request) {
         Optional<Transaction> transaction = transactionService.createTransaction(TransactionDto.fromRequest(request));
         if(transaction.isPresent()) {
             return new CreateTransactionSuccessResponse(transaction.get().getId());
