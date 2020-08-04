444050b933bfd759b398baa8fdcee9508ede3298
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-08-04 16:08:48 +0200

    Rename methods in TransactionService.

--- transaction-service/src/main/java/pl/adamboguszewski/transaction/service/application/TransactionService.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transaction/service/application/TransactionService.java
@@ -1,22 +1,20 @@
 package pl.adamboguszewski.transaction.service.application;
 
 import org.springframework.stereotype.Service;
-import pl.adamboguszewski.transaction.service.api.transaction.CreateTransactionRequest;
 import pl.adamboguszewski.transaction.service.application.dto.TransactionDto;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-// [TODO] Fill the details of this class.
 @Service
 public class TransactionService {
 
-    public List<Transaction> getAll() {
+    public List<Transaction> getAllTransactions() {
         return Collections.emptyList();
     }
 
-    public Optional<Transaction> getById(Long id) {
+    public Optional<Transaction> getByTransactionId(Long id) {
         return Optional.empty();
     }
 
