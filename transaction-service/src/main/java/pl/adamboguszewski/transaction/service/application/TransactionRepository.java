4e0c1f4280339769b2f96ce16cc40f8c169f613d
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-08-06 12:29:56 +0200

    Add findById method.

--- transaction-service/src/main/java/pl/adamboguszewski/transaction/service/application/TransactionRepository.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transaction/service/application/TransactionRepository.java
@@ -3,6 +3,10 @@ package pl.adamboguszewski.transaction.service.application;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long>{
+
+    Optional<Transaction> findById(Long id);
 }
