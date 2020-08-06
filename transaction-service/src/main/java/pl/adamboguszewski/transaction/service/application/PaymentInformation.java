e76f742eb0740934c63e7066c908a83018c75ea4
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-08-06 17:18:35 +0200

    Remodel PaymentInformation to be an Entity.

--- transaction-service/src/main/java/pl/adamboguszewski/transaction/service/application/PaymentInformation.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transaction/service/application/PaymentInformation.java
@@ -1,16 +1,32 @@
 package pl.adamboguszewski.transaction.service.application;
 
-import lombok.Value;
+import lombok.Data;
 import pl.adamboguszewski.transaction.service.api.Currency;
 import pl.adamboguszewski.transaction.service.api.PaymentType;
 import pl.adamboguszewski.transaction.service.application.dto.TransactionDto;
 
-@Value
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
 public class PaymentInformation {
 
+    @Id
+    Long id;
+
+    @ManyToOne
+    TransactionInformation transactionInformation;
+
+    @Column
     Long amountPaid;
+    @Column
     Long multiplier;
+    @Column
     Currency currency;
+    @Column
     PaymentType paymentType;
 
     public PaymentInformation(Long amountPaid, Long multiplier, Currency currency,
