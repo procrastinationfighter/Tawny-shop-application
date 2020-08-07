802c5d447b07fcf9c9b236a24b02936aa4bc81a5
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-08-07 12:17:01 +0200

    Move repository to infrastructure.

--- transaction-service/src/main/java/pl/adamboguszewski/transaction/service/infrastructure/repository/TransactionRepository.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transaction/service/infrastructure/repository/TransactionRepository.java
@@ -1,7 +1,8 @@
-package pl.adamboguszewski.transaction.service.application;
+package pl.adamboguszewski.transaction.service.infrastructure.repository;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.adamboguszewski.transaction.service.application.Transaction;
 
 import java.util.Optional;
 
