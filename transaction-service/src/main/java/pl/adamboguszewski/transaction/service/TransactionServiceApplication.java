da0dc0ffb5b2ab92680142d247574f5eb8637f43
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-08-12 13:30:52 +0200

    Remove imports

--- transaction-service/src/main/java/pl/adamboguszewski/transaction/service/TransactionServiceApplication.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transaction/service/TransactionServiceApplication.java
@@ -1,20 +1,7 @@
 package pl.adamboguszewski.transaction.service;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import pl.adamboguszewski.transaction.service.api.Currency;
-import pl.adamboguszewski.transaction.service.api.PaymentType;
-import pl.adamboguszewski.transaction.service.api.transaction.CreateTransactionRequest;
-import pl.adamboguszewski.transaction.service.application.TransactionService;
-import pl.adamboguszewski.transaction.service.application.dto.TransactionDto;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class TransactionServiceApplication {
