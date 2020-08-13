f1f2328cf6931ece9e197d8671b16fdb36b61e07
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-08-13 12:33:33 +0200

    Change TransactionNotFoundException

--- transaction-service/src/main/java/pl/adamboguszewski/transaction/service/application/TransactionNotFoundException.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transaction/service/application/TransactionNotFoundException.java
@@ -1,11 +1,19 @@
 package pl.adamboguszewski.transaction.service.application;
 
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+
 import java.util.UUID;
 
 // [TODO]:
+@EqualsAndHashCode(callSuper = true)
+@Value
 public class TransactionNotFoundException extends RuntimeException {
+
+    UUID id;
 
     public TransactionNotFoundException(UUID id) {
         super("Transaction with id: " + id + " not found.");
+        this.id = id;
     }
 }
