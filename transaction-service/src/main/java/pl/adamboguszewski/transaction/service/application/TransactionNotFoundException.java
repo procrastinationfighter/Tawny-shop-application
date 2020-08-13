4d26d76f020d95f95960790a3e6efa6ae887d05e
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-08-13 11:33:48 +0200

    Fill in TransactionNotFoundException details.

--- transaction-service/src/main/java/pl/adamboguszewski/transaction/service/application/TransactionNotFoundException.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transaction/service/application/TransactionNotFoundException.java
@@ -1,5 +1,11 @@
 package pl.adamboguszewski.transaction.service.application;
 
+import java.util.UUID;
+
 // [TODO]:
 public class TransactionNotFoundException extends RuntimeException {
+
+    public TransactionNotFoundException(UUID id) {
+        super("Transaction with id: " + id + " not found.");
+    }
 }
