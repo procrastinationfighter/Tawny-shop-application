f913991e822b539b96d29c116ba5f6441ad08e16
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-08-13 12:49:49 +0200

    Create ExceptionHandler when Transaction not found.

--- transaction-service/src/main/java/pl/adamboguszewski/transaction/service/infrastructure/exception/TransactionNotFoundExceptionHandler.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transaction/service/infrastructure/exception/TransactionNotFoundExceptionHandler.java
@@ -1,8 +1,20 @@
 package pl.adamboguszewski.transaction.service.infrastructure.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import pl.adamboguszewski.transaction.service.api.transaction.GetTransactionFailureResponse;
+import pl.adamboguszewski.transaction.service.api.transaction.GetTransactionResponse;
+import pl.adamboguszewski.transaction.service.application.TransactionNotFoundException;
 
 @ControllerAdvice
 public class TransactionNotFoundExceptionHandler {
 
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ExceptionHandler(TransactionNotFoundException.class)
+    public ResponseEntity<GetTransactionResponse> handle(TransactionNotFoundException exception) {
+        return new ResponseEntity<>(new GetTransactionFailureResponse(exception.getTransactionId()), HttpStatus.NOT_FOUND);
+    }
 }
