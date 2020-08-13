a3b7f50ae4bc492c534ab4e86bd5b95a711a672f
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-08-13 13:00:22 +0200

    Add logging in TransactionNotFoundExceptionHandler

--- transaction-service/src/main/java/pl/adamboguszewski/transaction/service/infrastructure/exception/TransactionNotFoundExceptionHandler.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transaction/service/infrastructure/exception/TransactionNotFoundExceptionHandler.java
@@ -1,5 +1,6 @@
 package pl.adamboguszewski.transaction.service.infrastructure.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,12 +10,16 @@ import pl.adamboguszewski.transaction.service.api.transaction.GetTransactionFail
 import pl.adamboguszewski.transaction.service.api.transaction.GetTransactionResponse;
 import pl.adamboguszewski.transaction.service.application.TransactionNotFoundException;
 
+@Slf4j
 @ControllerAdvice
 public class TransactionNotFoundExceptionHandler {
 
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     @ExceptionHandler(TransactionNotFoundException.class)
     public ResponseEntity<GetTransactionResponse> handle(TransactionNotFoundException exception) {
-        return new ResponseEntity<>(new GetTransactionFailureResponse(exception.getTransactionId()), HttpStatus.NOT_FOUND);
+        log.info("Transaction with id " + exception.getTransactionId() + " could not be found in the database.");
+        return new ResponseEntity<>(
+                new GetTransactionFailureResponse(exception.getTransactionId()),
+                HttpStatus.NOT_FOUND);
     }
 }
