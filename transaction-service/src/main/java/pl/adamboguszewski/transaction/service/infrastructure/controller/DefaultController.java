4452ba52e5d164bdbec86f81db8876ae93e0c6c8
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-08-17 12:05:26 +0200

    Add empty methods to DefaultController.

--- transaction-service/src/main/java/pl/adamboguszewski/transaction/service/infrastructure/controller/DefaultController.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transaction/service/infrastructure/controller/DefaultController.java
@@ -1,9 +1,22 @@
 package pl.adamboguszewski.transaction.service.infrastructure.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/transaction-service")
 public class DefaultController {
+
+    @GetMapping("")
+    public ResponseEntity<Void> getModuleInfo() {
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/api")
+    public ResponseEntity<Void> getApiInfo() {
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
 }
