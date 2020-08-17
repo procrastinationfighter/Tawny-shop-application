da8ab5719f7965b2e8b74c7e8a63c10d75743790
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-08-17 11:59:02 +0200

    Create default controller

---
+++ transaction-service/src/main/java/pl/adamboguszewski/transaction/service/infrastructure/controller/DefaultController.java
@@ -0,0 +1,9 @@
+package pl.adamboguszewski.transaction.service.infrastructure.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction-service")
+public class DefaultController {
+}
