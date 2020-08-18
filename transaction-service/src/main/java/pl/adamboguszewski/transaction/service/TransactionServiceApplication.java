7547951285ca1120b5f4dcf304fffa08f0e759f4
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-08-18 12:28:56 +0200

    Create months property.

--- transaction-service/src/main/java/pl/adamboguszewski/transaction/service/TransactionServiceApplication.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transaction/service/TransactionServiceApplication.java
@@ -2,8 +2,11 @@ package pl.adamboguszewski.transaction.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import pl.adamboguszewski.transaction.service.infrastructure.config.EmptyConfig;
 
 @SpringBootApplication
+@EnableConfigurationProperties(EmptyConfig.class)
 public class TransactionServiceApplication {
 
     public static void main(String[] args) {
--- transaction-service/src/main/java/pl/adamboguszewski/transaction/service/infrastructure/config/EmptyConfig.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transaction/service/infrastructure/config/EmptyConfig.java
@@ -1,4 +1,11 @@
 package pl.adamboguszewski.transaction.service.infrastructure.config;
 
+import lombok.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("transaction.months")
+@Value
 public class EmptyConfig {
+
+    Long months;
 }
