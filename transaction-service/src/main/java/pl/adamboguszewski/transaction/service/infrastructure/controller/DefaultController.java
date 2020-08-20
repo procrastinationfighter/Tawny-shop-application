ac904707e48bc4f271732be0ddbee4846b363487
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-08-20 16:28:05 +0200

    Inject build properties into DefaultController

--- transaction-service/src/main/java/pl/adamboguszewski/transaction/service/infrastructure/controller/DefaultController.java
+++ transaction-service/src/main/java/pl/adamboguszewski/transaction/service/infrastructure/controller/DefaultController.java
@@ -1,5 +1,6 @@
 package pl.adamboguszewski.transaction.service.infrastructure.controller;
 
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/transaction-service")
 public class DefaultController {
+
+    BuildProperties buildProperties;
+
+    public DefaultController(BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
 
     @GetMapping("")
     public ResponseEntity<Void> getModuleInfo() {
