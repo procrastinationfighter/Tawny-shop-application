21455488c72af02f24da06402cabf3951f3bd3a5
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-09-04 18:30:15 +0200

    Add member variables to service test class.

--- transaction-service/src/test/java/pl/adamboguszewski/transaction/service/test/TransactionServiceTest.java
+++ transaction-service/src/test/java/pl/adamboguszewski/transaction/service/test/TransactionServiceTest.java
@@ -1,13 +1,25 @@
 package pl.adamboguszewski.transaction.service.test;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import pl.adamboguszewski.transaction.service.application.Transaction;
+import pl.adamboguszewski.transaction.service.application.TransactionRepository;
+import pl.adamboguszewski.transaction.service.application.TransactionService;
+import pl.adamboguszewski.transaction.service.infrastructure.config.TransactionConfig;
 
 @SpringBootTest
 class TransactionServiceTest {
 
-    @Test
-    void contextLoads() {
-    }
+    @MockBean
+    TransactionRepository<Transaction> repository;
+
+    @MockBean
+    TransactionConfig config;
+
+    @InjectMocks
+    TransactionService service;
+
 
 }
